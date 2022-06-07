9155d9fd99a09ef7500e9736b974dfa614a24e3c
AValeriya <79207820+AValeriya@users.noreply.github.com>
2022-06-07 13:55:02 +0300

    new commit

--- WebBooks/catalog/tests.py
+++ WebBooks/catalog/tests.py
@@ -1,3 +1,19 @@
+from django.urls import reverse
+from django.contrib.auth.models import User
+from datetime import date
+from django.db import models
 from django.test import TestCase
 
 # Create your tests here.
+
+class TestAuthentification(TestCase):
+
+    def test_registration(self):
+        form_data = {'username': "Nina", 'password1': "qwer05350113", 'password2': "qwer05350113"}
+        response = self.client.post("/Register", data=form_data)
+        self.assertEqual(response.status_code, 302)
+
+    def test_login(self):
+        form_data = {'username': "Ngd", 'password': "qwer"}
+        response = self.client.post("/login", data=form_data)
+        self.assertEqual(response.status_code, 200)
